f67bede514c1ee0d6aca2caa654f3f83f852218e
EdwinCavazos <edwinalain911@gmail.com>
2022-09-09 00:20:21 -0500

    Se agrega el archivo de test en el que se puede crear un usuario y se manda a llamar el menu

---
+++ testMonedero.java
@@ -0,0 +1,17 @@
+package testMonedero;
+
+import monedero.*;
+
+
+
+public class testMonedero {
+	public static void main(String[] args) {
+		
+		
+		monedero Edwin = new monedero("Edwin", "Cavazos", 0d);
+		
+		Edwin.menu();
+			
+	
+}
+	}
